a3b541dcce79b54a7fe2e63b111d4d08025e2101
turboblaster44 <133587818+turboblaster44@users.noreply.github.com>
2024-04-15 14:59:52 +0200

    key generator

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import sys
 from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
 
 
 def pad_to_16_bytes(data):
@@ -10,26 +11,37 @@ def pad_to_16_bytes(data):
     return data + padding
 
 
-def encryptAES(pin, message):
-    if len(pin) % 16 != 0:
-        raise ValueError("Input data length must be a multiple of 16 bytes.")
+def encryptAES(pin, message: str):
+    if len(message) == 0:
+        raise ValueError("You must provide the key")
+
     pin_bytes = pin.encode('utf-8')
     message_bytes = message.encode('utf-8')
     message_bytes = pad_to_16_bytes(message_bytes)
 
     obj = AES.new(pin_bytes, AES.MODE_CBC, iv=b'1234123412341234')
     ciphertext = obj.encrypt(message_bytes)
-    print(ciphertext.hex())
-    return ciphertext
+    # print(ciphertext.hex())
+    return ciphertext.hex()
 
 
-def decryptAES(pin, cipherbytes):
+def decryptAES(pin, ciphertext):
     if len(pin) % 16 != 0:
-        raise ValueError("Input data length must be a multiple of 16 bytes.")
+        raise ValueError("PIN must be a multiple of 16 bytes.")
+    
+    cipherbytes = bytes.fromhex(ciphertext)
+    
     pin_bytes = pin.encode('utf-8')
 
     obj = AES.new(pin_bytes, AES.MODE_CBC, iv=b'1234123412341234')
     message = obj.decrypt(cipherbytes)
-    print(message)
-    message = message.rstrip(b'\x00') # trim 0 bytes 
+    # print(message)
+    message = message.rstrip(b'\x00')  # trim 0 bytes
     return message.decode("utf-8")
+
+
+def generateRSA():
+    key = RSA.generate(4096)
+    public_key = key.publickey().export_key()
+    private_key = key.export_key()
+    return private_key.decode(), public_key.decode()
--- window.py
+++ window.py
@@ -1,23 +1,52 @@
-from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QFileDialog
+from PyQt6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QFileDialog, QVBoxLayout, QTextEdit, QLineEdit
 from PyQt6.QtCore import QStandardPaths
+
+from utils import decryptAES, encryptAES, generateRSA
 
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.window_width, self.window_height = 400, 100
+        self.window_width, self.window_height = 600, 200
         self.setMinimumSize(self.window_width, self.window_height)
         self.initUI()
 
     def initUI(self):
         layout = QVBoxLayout()
-        self.setLayout(layout)
 
-        self.setWindowTitle('BSK')
-
-        button = QPushButton('Select File')
-        button.clicked.connect(self.chooseFile)
+        # Create a button
+        button = QPushButton('Generate RSA')
+        button.clicked.connect(self.generateRSA)
         layout.addWidget(button)
+
+        # Create a horizontal 2 text fields
+        text_layout = QHBoxLayout()
+        self.textF1 = QTextEdit()
+        self.textF2 = QTextEdit()
+        self.textF1.setReadOnly(True)
+        self.textF2.setReadOnly(True)
+        text_layout.addWidget(self.textF1)
+        text_layout.addWidget(self.textF2)
+        layout.addLayout(text_layout)
+
+        # Create a horizontal layout for the small text input and button
+        input_layout = QHBoxLayout()
+        self.text_input = QLineEdit()
+        self.text_input.setPlaceholderText("Enter 16 character PIN string")
+        input_layout.addWidget(self.text_input)
+        button2 = QPushButton('Encrypt')
+        button2.clicked.connect(self.encrypt)
+        input_layout.addWidget(button2)
+        layout.addLayout(input_layout)
+
+        # create output field
+        self.textF3 = QTextEdit()
+        self.textF3.setReadOnly(True)
+        layout.addWidget(self.textF3)
+
+        self.setLayout(layout)
+        self.setWindowTitle('Helper app')
+        self.show()
 
     def chooseFile(self):
         file_filter = 'PDF Files (*.pdf);;C++ Files (*.cpp)'
@@ -31,3 +60,18 @@ class Window(QWidget):
             filter=file_filter,
         )
         print(response)
+
+    def generateRSA(self):
+        private_key, public_key = generateRSA()
+        self.textF1.setPlainText(private_key)
+        self.textF2.setPlainText(public_key)
+
+    def encrypt(self):
+        pin = self.text_input.text()
+        private_key = self.textF1.toPlainText()
+        try:
+            encoded_key = encryptAES(pin, private_key)
+            self.textF3.setPlainText(encoded_key)
+        except ValueError as e:
+            self.textF3.setPlainText(str(e))
+            return
